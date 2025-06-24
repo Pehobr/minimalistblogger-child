8025853097b059c1c0817c9549aaa9355cffd617
Pehobr <79167920+Pehobr@users.noreply.github.com>
2025-06-24 06:01:30 +0000

    úprava kodu

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The template for displaying the footer
- *
+ * Když provedu změnu
  * Contains the closing of the #content div and all content after.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
