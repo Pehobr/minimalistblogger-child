70ddccbb3a306f0015e9cd21f4aa79f5323d9a20
Pehobr <pehobr@gmail.com>
2025-06-25 10:03:07 +0200

    DENNÍ KAPKY 1

--- functions.php
+++ functions.php
@@ -278,3 +278,158 @@ function pehobr_add_liturgical_color_body_class($classes) {
     return $classes;
 }
 add_filter('body_class', 'pehobr_add_liturgical_color_body_class');
+
+/**
+ * Registrace vlastního typu obsahu (Custom Post Type) pro denní citáty.
+ */
+function pehobr_register_daily_drops_cpt() {
+    $labels = array(
+        'name'                  => _x( 'Denní kapky', 'Post Type General Name', 'minimalistblogger-child' ),
+        'singular_name'         => _x( 'Denní kapka', 'Post Type Singular Name', 'minimalistblogger-child' ),
+        'menu_name'             => __( 'Denní kapky', 'minimalistblogger-child' ),
+        'name_admin_bar'        => __( 'Denní kapka', 'minimalistblogger-child' ),
+        'archives'              => __( 'Archiv Denních kapek', 'minimalistblogger-child' ),
+        'attributes'            => __( 'Atributy Denní kapky', 'minimalistblogger-child' ),
+        'parent_item_colon'     => __( 'Rodičovská položka:', 'minimalistblogger-child' ),
+        'all_items'             => __( 'Všechny Denní kapky', 'minimalistblogger-child' ),
+        'add_new_item'          => __( 'Přidat novou Denní kapku', 'minimalistblogger-child' ),
+        'add_new'               => __( 'Přidat novou', 'minimalistblogger-child' ),
+        'new_item'              => __( 'Nová Denní kapka', 'minimalistblogger-child' ),
+        'edit_item'             => __( 'Upravit Denní kapku', 'minimalistblogger-child' ),
+        'update_item'           => __( 'Aktualizovat Denní kapku', 'minimalistblogger-child' ),
+        'view_item'             => __( 'Zobrazit Denní kapku', 'minimalistblogger-child' ),
+        'view_items'            => __( 'Zobrazit Denní kapky', 'minimalistblogger-child' ),
+        'search_items'          => __( 'Hledat Denní kapku', 'minimalistblogger-child' ),
+    );
+    $args = array(
+        'label'                 => __( 'Denní kapka', 'minimalistblogger-child' ),
+        'description'           => __( 'Obsah pro denní zobrazení na úvodní stránce.', 'minimalistblogger-child' ),
+        'labels'                => $labels,
+        'supports'              => array( 'title', 'custom-fields' ), // Podporuje název a vlastní pole
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 5,
+        'menu_icon'             => 'dashicons-calendar-alt', // Ikonka v menu
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => false,
+        'exclude_from_search'   => true,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'post',
+        'show_in_rest'          => true,
+    );
+    register_post_type( 'denni_kapka', $args );
+}
+add_action( 'init', 'pehobr_register_daily_drops_cpt', 0 );
+
+/**
+ * Vytvoření vlastního REST API endpointu pro import Denních kapek.
+ * Endpoint bude dostupný na adrese: /wp-json/pkapky/v1/import
+ */
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'pkapky/v1', '/import', array(
+        'methods' => 'POST',
+        'callback' => 'pehobr_handle_daily_drops_import',
+        'permission_callback' => 'pehobr_import_permission_check'
+    ) );
+} );
+
+/**
+ * Bezpečnostní kontrola pro import.
+ * Povolí přístup pouze pokud požadavek obsahuje správný tajný klíč.
+ */
+function pehobr_import_permission_check( $request ) {
+    // --- ZDE NASTAVTE SVŮJ TAJNÝ KLÍČ ---
+    // Můžete si vymyslet jakýkoliv dlouhý a složitý řetězec.
+    $secret_key = 'TohleJedfs52985MujSuperTa665dfs2jnyKlicProMadfsdf6s8e2keDotCom';
+    // ------------------------------------
+    
+    $sent_key = $request->get_header('X-Import-Key'); // Klíč očekáváme v hlavičce požadavku
+
+    if ( $sent_key === $secret_key ) {
+        return true; // Klíče se shodují, přístup povolen
+    }
+    
+    // Pro debugování můžete odkomentovat následující řádek:
+    // error_log('Pokus o neautorizovaný import. Odeslaný klíč: ' . $sent_key);
+
+    return new WP_Error( 'rest_forbidden', 'Neplatný bezpečnostní klíč.', array( 'status' => 403 ) );
+}
+
+/**
+ * Funkce, která zpracuje data odeslaná z Make.com.
+ */
+function pehobr_handle_daily_drops_import( WP_REST_Request $request ) {
+    $items = $request->get_json_params();
+    $created_posts = 0;
+    $updated_posts = 0;
+
+    if ( empty( $items ) ) {
+        return new WP_Error( 'no_data', 'Nebyla přijata žádná data k importu.', array( 'status' => 400 ) );
+    }
+
+    // Projdeme všechny položky (řádky z tabulky) odeslané z Make.com
+    foreach ( $items as $item ) {
+        // Základní kontrola, zda máme potřebná data
+        if ( !isset( $item['datum'] ) || !isset( $item['nazev'] ) ) {
+            continue; // Přeskočíme položku, pokud nemá datum nebo název
+        }
+
+        // Připravíme data pro vytvoření nebo aktualizaci příspěvku
+        $post_args = array(
+            'post_type'    => 'denni_kapka',
+            'post_title'   => sanitize_text_field( $item['nazev'] ),
+            'post_content' => '', // Obsah nepotřebujeme
+            'post_status'  => 'publish',
+            'post_date'    => sanitize_text_field( $item['datum'] ) . ' 12:00:00', // Publikujeme v poledne daného dne
+        );
+
+        // Zkusíme najít, zda příspěvek s tímto datem již neexistuje
+        $existing_posts = get_posts(array(
+            'post_type' => 'denni_kapka',
+            'date_query' => array(
+                'year'  => substr($item['datum'], 0, 4),
+                'month' => substr($item['datum'], 5, 2),
+                'day'   => substr($item['datum'], 8, 2),
+            ),
+        ));
+
+        if ( $existing_posts ) {
+            // Příspěvek existuje, aktualizujeme ho
+            $post_id = $existing_posts[0]->ID;
+            $post_args['ID'] = $post_id;
+            wp_update_post( $post_args );
+            $updated_posts++;
+        } else {
+            // Příspěvek neexistuje, vytvoříme nový
+            $post_id = wp_insert_post( $post_args );
+            $created_posts++;
+        }
+
+        // Pokud byl příspěvek úspěšně vytvořen/aktualizován, uložíme vlastní pole (citáty)
+        if ( $post_id && !is_wp_error( $post_id ) ) {
+            // Projdeme všechny možné klíče citátů
+            $citat_keys = ['citat_janpavel', 'citat_benedikt', 'citat_frantisek', 'citat_modlitba', 'citat_lev', 'citat_citaty', 'citat_svatost', 'citat_texty', 'citat_komunita'];
+            foreach ($citat_keys as $key) {
+                if ( isset($item[$key]) ) {
+                    // Povolíme HTML kód, který chcete používat
+                    $allowed_html = [ 'p' => ['style' => []], 'em' => [], 'strong' => [], 'br' => [], 'span' => ['style' => []] ];
+                    $sanitized_value = wp_kses($item[$key], $allowed_html);
+                    update_post_meta( $post_id, $key, $sanitized_value );
+                }
+            }
+        }
+    }
+
+    // Vrátíme odpověď pro Make.com
+    return new WP_REST_Response(
+        array(
+            'status' => 'success',
+            'message' => 'Import dokončen.',
+            'created' => $created_posts,
+            'updated' => $updated_posts
+        ), 200 );
+}
--- page-home.php
+++ page-home.php
@@ -1,12 +1,83 @@
 <?php
 /**
  * Template Name: Úvodní stránka aplikace Home
- * Description: Speciální úvodní stránka pro aplikaci "Postní kapky" s mřížkou ikon a efektem kapek.
+ * Description: Speciální úvodní stránka, která dynamicky načítá denní obsah.
  *
  * @package minimalistblogger-child
  */
 
-get_header(); // Načte hlavičku WordPressu
+get_header();
+
+// --- ZDE ZAČÍNÁ NOVÁ LOGIKA PRO NAČÍTÁNÍ DENNÍHO OBSAHU ---
+
+$page_id_for_defaults = get_the_ID();
+$quotes = []; // Připravíme si prázdné pole pro citáty
+
+// --- NASTAVENÍ CYKLU ---
+// Zde nastavte datum Popeleční středy pro daný rok
+$start_date_str = '2026-02-18'; // POPELEČNÍ STŘEDA 2026
+
+try {
+    $start_date = new DateTime($start_date_str, new DateTimeZone('Europe/Prague'));
+    $today = new DateTime('today', new DateTimeZone('Europe/Prague'));
+
+    // Zjistíme, jestli jsme v období cyklu
+    if ($today >= $start_date) {
+        $interval = $start_date->diff($today);
+        $day_offset = $interval->days;
+
+        // Argumenty pro nalezení správného příspěvku v "Denních kapkách"
+        $args = array(
+            'post_type'      => 'denni_kapka', // Náš nový CPT
+            'post_status'    => 'publish',
+            'posts_per_page' => 1,
+            'offset'         => $day_offset,
+            'orderby'        => 'date',
+            'order'          => 'ASC',
+        );
+
+        $daily_query = new WP_Query($args);
+
+        if ($daily_query->have_posts()) {
+            while ($daily_query->have_posts()) {
+                $daily_query->the_post();
+                // Získáme ID nalezeného denního příspěvku
+                $daily_post_id = get_the_ID();
+            }
+            wp_reset_postdata();
+        }
+    }
+
+} catch (Exception $e) {
+    // V případě chyby v datu se nic nestane, použijí se výchozí hodnoty
+    $daily_post_id = null;
+}
+
+// Data pro dlaždice (zůstávají stejná)
+$grid_items = [
+    ['name' => 'Sv. Jan Pavel II', 'slug' => 'papez-frantisek', 'icon' => 'ikona-janpavel.png', 'citat_key' => 'citat_janpavel'],
+    ['name' => 'Papež Benedikt XVI.', 'slug' => 'papez-benedikt', 'icon' => 'ikona-benedikt.png', 'citat_key' => 'citat_benedikt'],
+    ['name' => 'Papež František', 'slug' => 'papez-frantisek', 'icon' => 'ikona-frantisek.png', 'citat_key' => 'citat_frantisek'],
+    ['name' => 'Modlitba', 'slug' => 'modlitba', 'icon' => 'ikona-modlitba.png', 'citat_key' => 'citat_modlitba'],
+    ['name' => 'Papež Lev XIV.', 'slug' => 'papez-lev', 'icon' => 'ikona-lev.png', 'citat_key' => 'citat_lev'],
+    ['name' => 'Fotocitát', 'slug' => 'citaty', 'icon' => 'ikona-citaty.png', 'citat_key' => 'citat_citaty'],
+    ['name' => 'Svatost', 'slug' => 'svatost', 'icon' => 'ikona-svatost.png', 'citat_key' => 'citat_svatost'],
+    ['name' => 'Augustin', 'slug' => 'nabozenske-texty', 'icon' => 'ikona-augustin.png', 'citat_key' => 'citat_texty'],
+    ['name' => 'Komunita', 'slug' => 'komunita', 'icon' => 'ikona-komunita.png', 'citat_key' => 'citat_komunita'],
+];
+
+// Naplníme pole citátů buď z denního příspěvku, nebo z výchozích hodnot na stránce
+foreach ($grid_items as $item) {
+    // Pokud máme ID denního příspěvku, použijeme ho
+    if (isset($daily_post_id)) {
+        $quotes[$item['citat_key']] = get_post_meta($daily_post_id, $item['citat_key'], true);
+    } else {
+        // Jinak použijeme výchozí hodnoty zadané na stránce "Home"
+        $quotes[$item['citat_key']] = get_post_meta($page_id_for_defaults, $item['citat_key'], true);
+    }
+}
+
+// --- KONEC NOVÉ LOGIKY ---
 ?>
 
 <div id="primary" class="featured-content content-area intro-app">
@@ -14,29 +85,11 @@ get_header(); // Načte hlavičku WordPressu
 
         <div id="intro-wrapper">
             <div id="intro-grid-container">
-                
                 <?php
-                $page_id = get_the_ID();
-                
-                // Data pro všechny dlaždice
-                $grid_items = [
-                    ['name' => 'Sv. Jan Pavel II', 'slug' => 'papez-frantisek', 'icon' => 'ikona-janpavel.png', 'citat_key' => 'citat_janpavel'],
-                    ['name' => 'Papež Benedikt XVI.', 'slug' => 'papez-benedikt', 'icon' => 'ikona-benedikt.png', 'citat_key' => 'citat_benedikt'],
-                    ['name' => 'Papež František', 'slug' => 'papez-frantisek', 'icon' => 'ikona-frantisek.png', 'citat_key' => 'citat_frantisek'],
-                    ['name' => 'Modlitba', 'slug' => 'modlitba', 'icon' => 'ikona-modlitba.png', 'citat_key' => 'citat_modlitba'],
-                    ['name' => 'Papež Lev XIV.', 'slug' => 'papez-lev', 'icon' => 'ikona-lev.png', 'citat_key' => 'citat_lev'],
-                    ['name' => 'Fotocitát', 'slug' => 'citaty', 'icon' => 'ikona-citaty.png', 'citat_key' => 'citat_citaty'],
-                    ['name' => 'Svatost', 'slug' => 'svatost', 'icon' => 'ikona-svatost.png', 'citat_key' => 'citat_svatost'],
-                    ['name' => 'Augustin', 'slug' => 'nabozenske-texty', 'icon' => 'ikona-augustin.png', 'citat_key' => 'citat_texty'],
-                    ['name' => 'Komunita', 'slug' => 'komunita', 'icon' => 'ikona-komunita.png', 'citat_key' => 'citat_komunita'],
-                ];
-
                 foreach ($grid_items as $item) :
-                    $quote = get_post_meta($page_id, $item['citat_key'], true);
-                    $has_quote = !empty($quote);
-                    // Odkaz bude buď '#' (pokud existuje citát) nebo na příslušnou stránku
+                    $quote_html = isset($quotes[$item['citat_key']]) ? $quotes[$item['citat_key']] : '';
+                    $has_quote = !empty($quote_html);
                     $link_url = $has_quote ? '#' : home_url('/' . $item['slug'] . '/');
-                    // <<<=== ZMĚNA ZDE: Používáme data-target-id místo data-quote ===>>>
                 ?>
                     <a href="<?php echo esc_url($link_url); ?>" 
                        class="icon-grid-item"
@@ -47,27 +100,19 @@ get_header(); // Načte hlavičku WordPressu
                         <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/img/' . $item['icon']); ?>" alt="<?php echo esc_attr($item['name']); ?>">
                     </a>
                 <?php endforeach; ?>
-
             </div>
         </div>
 
     </main>
 </div>
 
-<?php // <<<=== ZDE ZAČÍNÁ NOVÝ BLOK KÓDU ===>>> ?>
 <div id="hidden-quotes-container" style="display: none; visibility: hidden;">
     <?php
-    // Znovu projdeme položky a vygenerujeme skrytý obsah pro ty, které mají citát
     foreach ($grid_items as $item) :
-        $quote_html = get_post_meta($page_id, $item['citat_key'], true);
+        $quote_html = isset($quotes[$item['citat_key']]) ? $quotes[$item['citat_key']] : '';
         if (!empty($quote_html)) :
-            // Definujeme, které HTML značky a atributy jsou povolené
             $allowed_html = [
-                'p' => ['style' => []],
-                'em' => [],
-                'strong' => [],
-                'br' => [],
-                'span' => ['style' => []],
+                'p' => ['style' => []], 'em' => [], 'strong' => [], 'br' => [], 'span' => ['style' => []],
             ];
             ?>
             <div id="quote-content-<?php echo esc_attr($item['citat_key']); ?>">
@@ -78,14 +123,11 @@ get_header(); // Načte hlavičku WordPressu
     endforeach;
     ?>
 </div>
-<?php // <<<=== ZDE KONČÍ NOVÝ BLOK KÓDU ===>>> ?>
-
 
 <div id="quote-modal-overlay" class="quote-modal-overlay"></div>
 <div id="quote-modal-container" class="quote-modal-container">
     <button id="quote-modal-close-btn" class="quote-modal-close-btn">&times;</button>
-    <div id="quote-modal-content" class="quote-modal-content">
-        </div>
+    <div id="quote-modal-content" class="quote-modal-content"></div>
 </div>
 
-<?php get_footer(); // Načte patičku WordPressu ?>
+<?php get_footer(); ?>
