136023fe9403a34dabea7ed76aac18e890505dd1
Pehobr <pehobr@gmail.com>
2025-06-29 22:33:37 +0200

    total vymaz

--- README.md
+++
@@ -1,1 +0,0 @@
-# postni-kapky
--- js/dolni-lista.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-// Najde všechny texty (span) v odkazech lišty
-const navLinks = document.querySelectorAll('.bottom-nav-bar li a span');
-
-    // Projde všechny nalezené texty a skryje je
-    navLinks.forEach(span => {
-        span.style.display = 'none';
-    });
-});
