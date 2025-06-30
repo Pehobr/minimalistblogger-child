ec5eda14aa4263a71402efb088ef19b7b084795b
Pehobr <pehobr@gmail.com>
2025-06-30 06:44:56 +0200

    BOČNÍ MENU - SNAD FINAL

--- functions.php
+++ functions.php
@@ -2,6 +2,9 @@
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
 
+// Načtení souboru s logikou pro boční menu
+require_once( get_stylesheet_directory() . '/bocni-menu.php' );
+
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
@@ -22,3 +25,28 @@ endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
 // END ENQUEUE PARENT ACTION
+
+/**
+ * Načte styly a skripty pro vlastní boční menu.
+ */
+function moje_vlastni_menu_assets() {
+    // Načtení CSS pro boční menu
+    wp_enqueue_style(
+        'moje-bocni-menu-style',
+        get_stylesheet_directory_uri() . '/style/bocni-menu.css',
+        array(), // Závislosti, pokud nějaké jsou
+        filemtime(get_stylesheet_directory() . '/style/bocni-menu.css') // Verze souboru pro invalidaci cache
+    );
+
+    // Načtení JavaScriptu pro boční menu
+    wp_enqueue_script(
+        'moje-bocni-menu-script',
+        get_stylesheet_directory_uri() . '/js/bocni-menu.js',
+        array(), // Závislosti, např. 'jquery'
+        filemtime(get_stylesheet_directory() . '/js/bocni-menu.js'), // Verze souboru
+        true // Načíst ve patičce stránky
+    );
+}
+add_action( 'wp_enqueue_scripts', 'moje_vlastni_menu_assets' );
+
+?>
