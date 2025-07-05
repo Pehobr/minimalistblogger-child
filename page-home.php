459fde8de644a83ce002235bfd21a7448c93d769
Pehobr <pehobr@gmail.com>
2025-07-05 11:47:11 +0200

    úprava home2

--- page-home.php
+++ page-home.php
@@ -117,16 +117,16 @@ foreach ($grid_items as $item) {
             <div class="saints-section-container">
                 <div class="saints-images-wrapper">
                     <div class="saints-item">
-                        <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/img/ikona-augustin.png'); ?>" alt="sv. Augustin">
-                        <span class="grid-item-label">sv. Augustin</span>
+                        <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/img/ikona-augustin.png'); ?>" alt="Augustin">
+                        <span class="grid-item-label">Augustin</span>
                     </div>
                     <div class="saints-item">
                         <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/img/ikona-lev.png'); ?>" alt="Lev XIV.">
                         <span class="grid-item-label">Lev XIV.</span>
                     </div>
                     <div class="saints-item">
-                        <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/img/erb-lev.png'); ?>" alt="Svatý stolec">
-                        <span class="grid-item-label">Svatý stolec</span>
+                        <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/img/erb-lev.png'); ?>" alt="Vatikán">
+                        <span class="grid-item-label">Vatikán</span>
                     </div>
                 </div>
             </div>
