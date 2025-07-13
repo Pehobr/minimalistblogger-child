0ac435b81c144fdffd0e5c2482a9d84a3593da49
Pehobr <pehobr@gmail.com>
2025-07-13 16:09:20 +0200

    NASTAVENÍ MOBILNÍ LIŠTY - KÉŽ BY FINÁLNÍ

--- page-ikony-mobilni-listy.php
+++ page-ikony-mobilni-listy.php
@@ -18,78 +18,50 @@ get_header();
             <div class="entry-content">
                 <div id="mobile-nav-settings">
 
-                    <button class="accordion-btn">1. Ikona (vlevo)</button>
-                    <div class="accordion-content">
-                        <p class="settings-description">Vyberte, co se má zobrazit na první pozici v liště.</p>
-                        <div class="setting-item" data-nav-position="1">
-                            
-                            <label class="radio-label">
-                                <input type="radio" name="nav-icon-1" value="oblibene" data-icon="fa-star" data-url="<?php echo esc_url( home_url('/oblibene-texty/') ); ?>">
-                                <i class="fa fa-star"></i> Oblíbené texty
-                            </label>
+                    <?php
+                    // Pole s definicemi všech dostupných možností
+                    $icon_options = [
+                        ['value' => 'oblibene', 'icon' => 'fa-star', 'label' => 'Oblíbené texty', 'url' => home_url('/oblibene-texty/')],
+                        ['value' => 'archiv', 'icon' => 'fa-folder-open-o', 'label' => 'Archiv citátů', 'url' => home_url('/archiv-citatu/')],
+                        ['value' => 'zapisnik', 'icon' => 'fa-pencil', 'label' => 'Můj zápisník', 'url' => home_url('/zapisnik/')],
+                        ['value' => 'video', 'icon' => 'fa-video-camera', 'label' => 'Video kapky', 'url' => home_url('/video-kapky/')],
+                        ['value' => 'fotogalerie', 'icon' => 'fa-picture-o', 'label' => 'Fotogalerie', 'url' => home_url('/fotogalerie/')],
+                        ['value' => 'pisne', 'icon' => 'fa-music', 'label' => 'Postní písně', 'url' => home_url('/postni-pisne/')],
+                        ['value' => 'radia', 'icon' => 'fa-headphones', 'label' => 'Internetová rádia', 'url' => home_url('/krestanska-radia/')],
+                        ['value' => 'podcast', 'icon' => 'fa-podcast', 'label' => 'Podcast', 'url' => home_url('/podcast/')],
+                        ['value' => 'modlitba', 'icon' => 'fa-commenting', 'label' => 'Modlitba', 'url' => home_url('/')],
+                        ['value' => 'poboznost', 'icon' => 'fa-book', 'label' => 'Pobožnost', 'url' => home_url('/poboznosti/')],
+                    ];
 
-                            <label class="radio-label">
-                                <input type="radio" name="nav-icon-1" value="archiv" data-icon="fa-folder-open-o" data-url="<?php echo esc_url( home_url('/archiv-citatu/') ); ?>">
-                                <i class="fa fa-folder-open-o"></i> Archiv citátů
-                            </label>
+                    // Definice tlačítek (pozic), které chceme zobrazit
+                    $positions = [
+                        1 => '1. Ikona (vlevo)',
+                        2 => '2. Ikona',
+                        4 => '4. Ikona',
+                        5 => '5. Ikona (vpravo)',
+                    ];
 
-                            <label class="radio-label">
-                                <input type="radio" name="nav-icon-1" value="zapisnik" data-icon="fa-pencil" data-url="<?php echo esc_url( home_url('/zapisnik/') ); ?>">
-                                <i class="fa fa-pencil"></i> Můj zápisník
-                            </label>
+                    foreach ($positions as $pos_num => $pos_label) :
+                    ?>
+                        <button class="accordion-btn"><?php echo $pos_label; ?></button>
+                        <div class="accordion-content">
+                            <p class="settings-description">Vyberte, co se má zobrazit na <?php echo str_replace(['Ikona', '(vlevo)', '(vpravo)'], ['pozici', '', ''], $pos_label); ?> v liště.</p>
+                            <div class="setting-item" data-nav-position="<?php echo $pos_num; ?>">
+                                
+                                <?php foreach ($icon_options as $option) : ?>
+                                <label class="radio-label">
+                                    <input type="radio" 
+                                           name="nav-icon-<?php echo $pos_num; ?>" 
+                                           value="<?php echo esc_attr($option['value']); ?>" 
+                                           data-icon="<?php echo esc_attr($option['icon']); ?>" 
+                                           data-url="<?php echo esc_url($option['url']); ?>">
+                                    <i class="fa <?php echo esc_attr($option['icon']); ?>"></i> <?php echo esc_html($option['label']); ?>
+                                </label>
+                                <?php endforeach; ?>
 
-                            <label class="radio-label">
-                                <input type="radio" name="nav-icon-1" value="video" data-icon="fa-video-camera" data-url="<?php echo esc_url( home_url('/video-kapky/') ); ?>">
-                                <i class="fa fa-video-camera"></i> Video kapky
-                            </label>
-
-                            <label class="radio-label">
-                                <input type="radio" name="nav-icon-1" value="fotogalerie" data-icon="fa-picture-o" data-url="<?php echo esc_url( home_url('/fotogalerie/') ); ?>">
-                                <i class="fa fa-picture-o"></i> Fotogalerie
-                            </label>
-
-                            <label class="radio-label">
-                                <input type="radio" name="nav-icon-1" value="pisne" data-icon="fa-music" data-url="<?php echo esc_url( home_url('/postni-pisne/') ); ?>">
-                                <i class="fa fa-music"></i> Postní písně
-                            </label>
-
-                            <label class="radio-label">
-                                <input type="radio" name="nav-icon-1" value="radia" data-icon="fa-headphones" data-url="<?php echo esc_url( home_url('/krestanska-radia') ); ?>">
-                                <i class="fa fa-headphones"></i> Internetová rádia
-                            </label>
-
-                            <label class="radio-label">
-                                <input type="radio" name="nav-icon-1" value="podcast" data-icon="fa-podcast" data-url="<?php echo esc_url( home_url('/podcast/') ); ?>">
-                                <i class="fa fa-podcast"></i> Podcast
-                            </label>
-
-                             <label class="radio-label">
-                                <input type="radio" name="nav-icon-1" value="modlitba" data-icon="fa-commenting" data-url="<?php echo esc_url( home_url('/') ); ?>">
-                                <i class="fa fa-commenting"></i> Modlitba
-                            </label>
-
-                             <label class="radio-label">
-                                <input type="radio" name="nav-icon-1" value="poboznost" data-icon="fa-book" data-url="<?php echo esc_url( home_url('/poboznosti/') ); ?>">
-                                <i class="fa fa-book"></i> Pobožnost
-                            </label>
-
+                            </div>
                         </div>
-                    </div>
-
-                    <button class="accordion-btn">2. Ikona</button>
-                    <div class="accordion-content">
-                        <p>Nastavení pro druhou ikonu bude doplněno.</p>
-                    </div>
-
-                    <button class="accordion-btn">4. Ikona</button>
-                    <div class="accordion-content">
-                         <p>Nastavení pro čtvrtou ikonu bude doplněno.</p>
-                    </div>
-
-                    <button class="accordion-btn">5. Ikona (vpravo)</button>
-                    <div class="accordion-content">
-                         <p>Nastavení pro pátou ikonu bude doplněno.</p>
-                    </div>
+                    <?php endforeach; ?>
 
                 </div>
             </div>
